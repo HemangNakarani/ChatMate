732dc939e1533f475896e29514672be71ccf6653
devsoftzz <devsoft.deven@gmail.com>
2019-12-21 11:57:43 +0530

    Auth Added

--- app/src/main/java/com/hemangnh18/chatmate/MainActivity.java
+++ app/src/main/java/com/hemangnh18/chatmate/MainActivity.java
@@ -1,46 +1,40 @@
 package com.hemangnh18.chatmate;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
-import com.hemangnh18.authentication.Auth_It;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        final FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser==null){
+            startActivity(new Intent(this,AuthenticationActivity.class));
+            finish();
+        }
+
+        Button logout = findViewById(R.id.out);
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Auth_It auth_it = new Auth_It(MainActivity.this);
-                auth_it.Authenticate();
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this,AuthenticationActivity.class));
+                finish();
             }
         });
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode==Auth_It.AUTH_KEY)
-        {
-                if(resultCode==RESULT_OK)
-                {
-                    Toast.makeText(MainActivity.this,"Abey Saale...",Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Toast.makeText(MainActivity.this,"Tameez se...",Toast.LENGTH_LONG).show();
-                }
-        }
     }
 }
