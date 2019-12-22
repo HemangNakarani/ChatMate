5261ba36cb5a4fafc09a10066bd1a9e1cedc9fc6
Devsoftzz <devsoft.deven@gmail.com>
2019-12-22 10:58:09 +0530

    Bottom Navigation Bar

--- app/src/main/java/com/hemangnh18/chatmate/MainActivity.java
+++ app/src/main/java/com/hemangnh18/chatmate/MainActivity.java
@@ -2,11 +2,15 @@ package com.hemangnh18.chatmate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -22,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         final FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser==null){
+        /*if(currentUser==null){
             startActivity(new Intent(this,AuthenticationActivity.class));
             finish();
-        }
+        }*/
 
         Button logout = findViewById(R.id.out);
         logout.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +40,36 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_contact:
+                            selectedFragment = new ContactFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
