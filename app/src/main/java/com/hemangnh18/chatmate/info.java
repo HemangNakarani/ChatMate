2cbeb8fcb33065f7044d8799a9404abe863e336d
devsoftzz <devsoft.deven@gmail.com>
2019-12-24 21:38:41 +0530

    Info Activity Added

---
+++ app/src/main/java/com/hemangnh18/chatmate/info.java
@@ -0,0 +1,37 @@
+package com.hemangnh18.chatmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+public class info extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TextView mStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        mToolbar = findViewById(R.id.toolbar);
+        //TODO
+        //mToolbar.setTitle();
+
+        mStatus = findViewById(R.id.status);
+        //TODO
+        //mStatus.setText();
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
