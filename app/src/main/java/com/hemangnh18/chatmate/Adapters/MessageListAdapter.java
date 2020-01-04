5bfe08c4b063c901f5a53579a591965fb0dc07e2
pritsahkar <pritsahkar2000@gmail.com>
2020-01-05 02:59:00 +0530

    Working on Emoji

--- app/src/main/java/com/hemangnh18/chatmate/Adapters/MessageListAdapter.java
+++ app/src/main/java/com/hemangnh18/chatmate/Adapters/MessageListAdapter.java
@@ -1,12 +1,14 @@
 package com.hemangnh18.chatmate.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -106,7 +108,7 @@ public class MessageListAdapter extends RecyclerView.Adapter {
         }
 
         void bind(SocketMessage message,int pos) {
-            messageText.setText(message.getMessage());
+
             timeText.setText(message.getTime());
             if(pos==mMessageList.size()-1)
             {
@@ -117,6 +119,22 @@ public class MessageListAdapter extends RecyclerView.Adapter {
                 msgStatus.setVisibility(View.GONE);
             }
             UpdateMsgStatus(msgStatus);
+
+            if(isEmoji(message.getMessage()))
+            {
+                messageText.setBackgroundColor(Color.argb(0,0,0,0));
+                messageText.setEmojiSize(120);
+                if(isEmojiis(message.getMessage()))
+                {
+                    messageText.setEmojiSize(300);
+                }
+            }
+            else
+            {
+                messageText.setBackground(ContextCompat.getDrawable(mContext,R.drawable.rounded_sent));
+                messageText.setEmojiSize(60);
+            }
+            messageText.setText(message.getMessage());
 
         }
     }
@@ -135,7 +153,7 @@ public class MessageListAdapter extends RecyclerView.Adapter {
         }
 
         void bind(SocketMessage message,int position) {
-            messageText.setText(message.getMessage());
+
             timeText.setText(message.getTime());
             nameText.setText(oppositeUser.getUSERNAME_IN_PHONE());
 
@@ -151,6 +169,22 @@ public class MessageListAdapter extends RecyclerView.Adapter {
                     profileImage.setVisibility(View.VISIBLE);
                 }
             }
+
+            if(isEmoji(message.getMessage()))
+            {
+                messageText.setBackgroundColor(Color.argb(0,0,0,0));
+                messageText.setEmojiSize(120);
+                if(isEmojiis(message.getMessage()))
+                {
+                    messageText.setEmojiSize(300);
+                }
+            }
+            else
+            {
+                messageText.setBackground(ContextCompat.getDrawable(mContext,R.drawable.chat_message_rounded));
+                messageText.setEmojiSize(60);
+            }
+            messageText.setText(message.getMessage());
         }
     }
 
@@ -169,5 +203,21 @@ public class MessageListAdapter extends RecyclerView.Adapter {
 
             }
         });
+    }
+    private static boolean isEmoji(String message){
+        return message.matches("(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|" +
+                "[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|" +
+                "[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|" +
+                "[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|" +
+                "[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|" +
+                "[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|" +
+                "[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|" +
+                "[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|" +
+                "[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|" +
+                "[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|" +
+                "[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)+");
+    }
+    private static boolean isEmojiis(String message){
+        return message.matches("(?:[\u2764\uFE0F\u2665]){2}");
     }
 }
