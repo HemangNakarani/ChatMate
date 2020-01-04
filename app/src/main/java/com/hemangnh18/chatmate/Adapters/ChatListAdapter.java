56ea860aff18dc1f576fa05a9f3558c6b75d53ae
devsoftzz <devsoft.deven@gmail.com>
2020-01-04 17:57:44 +0530

    Merge branch 'master' of E:\Apps\Unfinished\ChatMate with conflicts.

---
+++ app/src/main/java/com/hemangnh18/chatmate/Adapters/ChatListAdapter.java
@@ -0,0 +1,139 @@
+package com.hemangnh18.chatmate.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.tntkhang.fullscreenimageview.library.FullScreenImageViewActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.hemangnh18.chatmate.Classes.Methods;
+import com.hemangnh18.chatmate.Classes.Room;
+import com.hemangnh18.chatmate.Classes.User;
+import com.hemangnh18.chatmate.Compressing.Converter;
+import com.hemangnh18.chatmate.Database.DatabaseHandler;
+import com.hemangnh18.chatmate.ImageViewer.FullScreenImageViewActivity2;
+import com.hemangnh18.chatmate.MainActivity;
+import com.hemangnh18.chatmate.MessageActivity;
+import com.hemangnh18.chatmate.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUsers;
+
+    public ChatListAdapter(Context mContext,List<User> users)
+    {
+        this.mContext= mContext;
+        this.mUsers = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_contacts_layout,parent,false);
+        return new ChatListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+
+        final int k = position;
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it= new Intent(mContext,MessageActivity.class);
+                it.putExtra("Opposite",mUsers.get(k).getUSER_ID());
+                mContext.startActivity(it);
+
+            }
+        });
+
+        holder.username.setText(mUsers.get(k).getUSERNAME_IN_PHONE());
+
+        if(mUsers.get(k).getBASE64().equals("Default"))
+        {
+            if(mUsers.get(k).getGENDER().equals("Male"))
+            {
+                holder.dp.setImageResource(R.drawable.man);
+            }
+            else
+            {
+                holder.dp.setImageResource(R.drawable.girl);
+            }
+        }
+        else
+        {
+            holder.dp.setImageBitmap(Converter.Base642Bitmap(mUsers.get(k).getBASE64()));
+        }
+
+        holder.dp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dwn = mUsers.get(k).getDOWNLOAD();
+                if(dwn.equals("Default"))
+                {
+
+                }
+                else
+                {
+                    Intent fullImageIntent = new Intent(mContext, FullScreenImageViewActivity2.class);
+                    ArrayList<String> uriString = new ArrayList<>();
+                    uriString.add(dwn);
+                    fullImageIntent.putExtra(FullScreenImageViewActivity.URI_LIST_DATA, uriString);
+                    fullImageIntent.putExtra(FullScreenImageViewActivity2.URI_DOWNLOAD_DATA, uriString);
+                    fullImageIntent.putExtra(FullScreenImageViewActivity.IMAGE_FULL_SCREEN_CURRENT_POS, 0);
+                    mContext.startActivity(fullImageIntent);
+                }
+            }
+        });
+        //ToDO
+        holder.lastMessage.setText(mUsers.get(k).getSTATUS());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView username,lastMessage,timestamp;
+        private CircleImageView dp,dot;
+        private CardView cardView;
+
+        public ViewHolder(View itemView)
+        {
+            super(itemView);
+            username = itemView.findViewById(R.id.username_contact_item);
+            lastMessage = itemView.findViewById(R.id.message_contact_item);
+            dp = itemView.findViewById(R.id.contacts_dp);
+            cardView = itemView.findViewById(R.id.contacts_card);
+            dot = itemView.findViewById(R.id.pending_message_circleImageView);
+            timestamp = itemView.findViewById(R.id.timestamp_contact_item);
+        }
+    }
+
+}
--- app/src/main/java/com/hemangnh18/chatmate/info.java
+++ app/src/main/java/com/hemangnh18/chatmate/info.java
@@ -3,30 +3,26 @@ package com.hemangnh18.chatmate;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.hemangnh18.chatmate.Classes.User;
-import com.hemangnh18.chatmate.Database.DatabaseHandler;
 
 public class info extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private TextView mStatus;
-    private ImageView mDp;
     private TextView mPhonenumber;
     private SwitchCompat mSwitchCompact;
     private LinearLayout mClearChat;
     private LinearLayout mBlock;
-    private DatabaseHandler databaseHandler;
+    private ConstraintLayout mDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +30,12 @@ public class info extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         mToolbar = findViewById(R.id.profile_toolbar);
-        mDp = findViewById(R.id.header);
         mStatus = findViewById(R.id.profile_status);
         mPhonenumber = findViewById(R.id.profile_phonenumber);
         mSwitchCompact = findViewById(R.id.profile_notification);
         mClearChat = findViewById(R.id.profile_clearchat);
         mBlock = findViewById(R.id.profile_block);
-        databaseHandler= new DatabaseHandler(info.this);
+        mDelete= findViewById(R.id.delete);
 
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,13 +45,10 @@ public class info extends AppCompatActivity {
         });
 
         if(getIntent().hasExtra("USER")){
-            String oppositeUID = getIntent().getStringExtra("USER");
-            User oppositeUser = databaseHandler.getUser(oppositeUID);
+            User oppositeUser = getIntent().getParcelableExtra("USER");
             mToolbar.setTitle(oppositeUser.getUSERNAME_IN_PHONE());
             mStatus.setText(oppositeUser.getSTATUS());
             mPhonenumber.setText(oppositeUser.getPHONE());
-            Glide.with(this).load(oppositeUser.getDOWNLOAD()).into(mDp);
-
         }
 
         //todo
